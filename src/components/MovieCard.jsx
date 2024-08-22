81eb36739b10cafe337ce49e97105a7a39639b2c
Art_Twis <34947496+ArtTwis@users.noreply.github.com>
2024-08-23 03:32:31 +0530

    add blur effect on image until its not load

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { postersArray } from "../utils/imageArray.js";
 import { randomNumber, randomColor } from "../utils/random.js";
 import { randomText } from "../constant/constant.js";
 
 const MovieCard = ({ myData }) => {
   const { title, fullplot } = myData;
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setIsImageLoaded(true);
+  };
 
   return (
     <div className="card box block">
       <div className="card-info">
         <div className="imageContainer" style={{ background: randomColor() }}>
-          <img src={postersArray[randomNumber()]} alt="Poster" />
+          <img
+            src={postersArray[randomNumber()]}
+            alt="Poster"
+            style={{
+              filter: isImageLoaded ? "none" : "blur(10px)",
+              transition: "filter 0.5s ease",
+            }}
+            onLoad={handleImageLoad}
+          />
         </div>
         <h2>{title}</h2>
         <p>{fullplot ? fullplot : randomText}</p>
